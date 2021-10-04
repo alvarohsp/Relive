553dd5e2b37eae9f1f1f0e677566a71c4e75bcde
alvarohsp <alvaro.hpandelo@gmail.com>
2021-10-03 23:54:25 -0300

    0.1.2-PREVIEW - Comando Play adicionado como Slash Command, deve reproduzir as músicas com mais estabilidade

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,15 +1,84 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { joinVoiceChannel,
+    createAudioPlayer,
+    NoSubscriberBehavior,
+    createAudioResource } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
         .setDescription('Play youtube music!')
-        .addStringOption(option => 
-            option.setName('URL')
+        .addStringOption(option =>
+            option.setName('url')
                 .setDescription('Song name of URL')
                 .setRequired(true)),
     async execute(interaction) {
-        console.log(interaction);
-       
+        await interaction.deferReply();
+        await playMusic(interaction);
+        await interaction.editReply('Pronto!');
     }
+
 };
+
+async function playMusic(interaction) {
+    
+    const voiceChannel = interaction.member.voice.channel;
+    const url = interaction.options.getString('url');
+
+    if (!voiceChannel) return interaction.reply('Entre em um canal de voz');
+    if (!url) return interaction.reply('Tocar o que?');
+
+    const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
+
+    const player = createAudioPlayer({
+        behaviors: {
+            NoSubscriber: NoSubscriberBehavior.Stop,
+        }
+    });
+
+    let song;
+
+    if (ytdl.validateURL(url)) {
+        console.log('youtubeLink');
+
+        const songInfo = await ytdl.getInfo(url);
+        song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            chapters: songInfo.videoDetails.chapters
+        };
+        // console.log(songInfo);
+
+    } else {
+
+        const { videos } = await ytSearch(url);
+
+        if (!videos.length) return interaction.reply('Música não encontrada!');
+
+        const songInfo = await ytdl.getInfo(videos[0].url);
+        song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            chapters: songInfo.videoDetails.chapters
+        };
+    }
+
+    if (song.url) {
+        const stream = ytdl(song.url, { filter: 'audio', quality: 'highestaudio', liveBuffer: 0, highWaterMark: 32000 });
+        const resource = createAudioResource(stream);
+        const subscription = connection.subscribe(player);
+        player.play(resource);
+
+    } else {
+        interaction.reply('Música não encontrada!');
+        return;
+    }
+
+}
+        
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,9 +7,9 @@ const {
     NoSubscriberBehavior,
     createAudioResource
 } = require('@discordjs/voice');
-const play = require('play-dl');
-// const ytdl = require('ytdl-core');
-// const ytSearch = require('yt-search');
+// const play = require('play-dl');
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
 
 module.exports = {
     name: 'messageCreate',
@@ -36,24 +36,29 @@ module.exports = {
         if (!args.length) return message.channel.send('Play what?');
 
         // create a connection, joining a voice channel the message sender is in.
-        const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: voiceChannel.guild.id,
-            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-        });
+        // const connection = joinVoiceChannel({
+        //     channelId: voiceChannel.id,
+        //     guildId: voiceChannel.guild.id,
+        //     adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        // });
 
-        const ytInfo = await play.search(args.join(' '), { limit: 1 });
-        const stream = await play.stream(ytInfo[0].url, { quality: 'highestaudio'});
-        const resource = createAudioResource(stream.stream, { inputType: stream.type});
+        // const ytInfo = await play.search(args.join(' '), { limit: 1 });
+        // console.log('ytInfo: ', ytInfo);
 
-        const player = createAudioPlayer({
-            behaviors: {
-                NoSubscriber: NoSubscriberBehavior.Play,
-            }
-        });
+        // const stream = await play.stream(ytInfo[0].url, { quality: 'highestaudio'});
+        // console.log(args);
+        // const stream = await play.stream(args[0]);
+        
+        // const resource = createAudioResource(stream.stream, { inputType: stream.type});
 
-        player.play(resource);
-        connection.subscribe(player);
+        // const player = createAudioPlayer({
+        //     behaviors: {
+        //         NoSubscriber: NoSubscriberBehavior.Play,
+        //     }
+        // });
+
+        // player.play(resource);
+        // connection.subscribe(player);
 
         //////////////////////////////////////////////////////////////////////////////////////////////////
         //////////////////////////
@@ -62,60 +67,56 @@ module.exports = {
         ///////
         //
 
+        // create a connection, joining a voice channel the message sender is in.
+        const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        });
 
-        // const voiceChannel = message.member.voice.channel;
+        connection.on('stateChange', (oldState, newState) => {
+            console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+        });
 
-        // if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play');
-        // if (!args.length) return message.channel.send('Play what?');
+        // create an audio player
+        const player = createAudioPlayer({
+            behaviors: {
+                NoSubscriber: NoSubscriberBehavior.Stop,
+            }
+        });
 
-        // // create a connection, joining a voice channel the message sender is in.
-        // const connection = joinVoiceChannel({
-        //     channelId: voiceChannel.id,
-        //     guildId: voiceChannel.guild.id,
-        //     adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-        // });
-
-        // connection.on('stateChange', (oldState, newState) => {
-        //     console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
-        // });
-
-        // // create an audio player
-        // const player = createAudioPlayer({
-        //     behavoirs: {
-        //         NoSubscriber: NoSubscriberBehavior.Pause,
-        //     }
-        // });
-
-        // // create an audio resource from Youtube
-        // const videoFinder = async (query) => {
-        //     const videoResult = await ytSearch(query);
-        //     return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
-        // };
+        // create an audio resource from Youtube
+        const videoFinder = async (query) => {
+            const videoResult = await ytSearch(query);
+            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+        };
 
         // const video = await videoFinder(args.join(' '));
-        // if (video) {
-        //     // get a readable stream
-        //     const stream = ytdl(video.url, { filter: 'audioonly' });
-        //     const resource = createAudioResource(stream);
-        //     // const resource = createAudioResource(join(__dirname, 'test.mp3')); //////////////// This does not work
+        const video = {
+            url: args.join(' ')
+        };
+        if (video) {
+            // get a readable stream
+            const stream = ytdl(video.url, { filter: 'audio', liveBuffer: 0, highWaterMark: 32000});
+            const resource = createAudioResource(stream);
+            // const resource = createAudioResource(join(__dirname, 'test.mp3')); //////////////// This does not work
 
-        //     // playing audio through subscription of an audioplayer
-        //     const subscription = connection.subscribe(player);
+            // playing audio through subscription of an audioplayer
+            // eslint-disable-next-line no-unused-vars
+            const subscription = connection.subscribe(player);
 
-        //     //setup player
-        //     player.play(resource);
-        //     message.reply(`Now Playing ${video.title}`);
+            //setup player
+            player.play(resource);
+            message.reply(`Now Playing ${video.title}`);
 
-        //     console.log(subscription);
-
-        //     player.on('stateChange', (oldState, newState) => {
-        //         console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
-        //     });
-        // }
-        // else {
-        //     message.channel.send('Video not found');
-        //     return;
-        // }
+            player.on('stateChange', (oldState, newState) => {
+                console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+            });
+        }
+        else {
+            message.channel.send('Video not found');
+            return;
+        }
 
     }
 }
